42c42b3ed07d4617cf777f825fdc4b39189003c7
emsfoit <emsfoit@gmail.com>
2023-12-06 23:11:26 +0100

    Add loading and error handling to TasksList component

--- Frontend/src/views/HomeView.vue
+++ Frontend/src/views/HomeView.vue
@@ -15,7 +15,9 @@
       <Icon :icon="formIcon" :style="{}" @click="toggleCreateForm" />
     </h2>
     <TasksCreate v-if="showCreateForm"></TasksCreate>
-    <TasksList> </TasksList>
+    <div v-if="loading"> Looooading</div>
+    <div v-if="error"> {{ error }}</div>
+    <TasksList v-else /> 
   </main>
 </template>
 
@@ -25,14 +27,31 @@ import TasksSearch from '@/components/Tasks/TasksSearch.vue'
 import TasksCreate from '@/components/Tasks/TasksCreate.vue'
 import { ref, computed } from 'vue'
 import { Icon } from '@iconify/vue'
+import { useTaskStore } from '@/stores/tasks'
+// LOAD TASKS
+const tasksStore = useTaskStore()
 
 const showCreateForm = ref(false)
+const loading = ref(true)
+const error = ref(null)
 const toggleCreateForm = () => {
   showCreateForm.value = !showCreateForm.value
 }
 const formIcon = computed(() => {
   return showCreateForm.value ? 'ph:minus-fill' : 'ph:plus-fill'
 })
+
+tasksStore
+  .fetchTasks()
+  .then(() => {
+    error.value = null
+  })
+  .catch((err) => {
+    error.value = err
+  })
+  .finally(() => {
+    loading.value = false
+  })
 </script>
 
 <style scoped>
