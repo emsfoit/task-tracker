1315657774693b5f6f92a7ca9afd15dff77b1142
emsfoit <emsfoit@gmail.com>
2023-12-06 21:36:38 +0100

    add e2e test for task

--- Backend/test/app.e2e-spec.ts
+++ Backend/test/app.e2e-spec.ts
@@ -1,24 +1,99 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import { Test } from '@nestjs/testing';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { AppModule } from '../src/app.module';
+import { PrismaService } from '../src/prisma/prisma.service';
+import { TaskDto, TaskStatus } from '../src/task/dto';
+import * as pactum from 'pactum';
+
+const APP_URL = 'http://localhost:3334';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
-
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+  let prisma: PrismaService;
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
-
-    app = moduleFixture.createNestApplication();
+    app = moduleRef.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+      }),
+    );
     await app.init();
+    await app.listen(3334);
+
+    prisma = app.get(PrismaService);
+
+    await prisma.cleanDb();
+
+    pactum.request.setBaseUrl(APP_URL);
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await prisma.$disconnect();
+    await app.close();
+  });
+
+  describe('Task', () => {
+    const dto: TaskDto = {
+      title: 'New Task',
+      description: 'new task description',
+      status: TaskStatus.OPEN,
+    };
+
+    describe('Create a task', () => {
+      it('should create a task', () => {
+        return pactum
+          .spec()
+          .post('/tasks')
+          .withBody(dto)
+          .expectStatus(201)
+          .stores('taskId', 'id');
+      });
+    });
+
+    describe('Get all tasks', () => {
+      it('should get all tasks', () => {
+        return pactum.spec().get('/tasks').expectStatus(200).inspect();
+      });
+    });
+
+    describe('Get a task', () => {
+      it('should get a task', () => {
+        return pactum
+          .spec()
+          .get('/tasks/{taskId}')
+          .withPathParams('taskId', '$S{taskId}')
+          .expectStatus(200)
+          .inspect();
+      });
+    });
+
+    describe('update a task', () => {
+      const dto: TaskDto = {
+        title: 'New Task',
+        description: 'new task description',
+        status: TaskStatus.OPEN,
+      };
+      it('should update a task', () => {
+        return pactum
+          .spec()
+          .patch('/tasks/{taskId}')
+          .withPathParams('taskId', '$S{taskId}')
+          .withBody(dto)
+          .expectStatus(200);
+      });
+    });
+
+    describe('Delete a task', () => {
+      it('Should delete a task', () => {
+        return pactum
+          .spec()
+          .delete('/tasks/{taskId}')
+          .withPathParams('taskId', '$S{taskId}')
+          .expectStatus(200);
+      });
+    });
   });
 });
